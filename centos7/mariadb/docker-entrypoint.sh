8a0fdfae150447ab6b231dc094d906805d12f888
Nicholas Alipaz <nalipaz@labiomed.org>
2016-06-02 15:00:44 -0700

    rework db entrypoint

--- centos7/mariadb/docker-entrypoint.sh
+++ centos7/mariadb/docker-entrypoint.sh
@@ -1,89 +1,65 @@
 #!/bin/bash
-set -e
 
-  # Get config
-  DATADIR="/var/lib/mysql"
-  echo $DATADIR
+VOLUME_HOME="/var/lib/mysql"
 
-if [ ! -d "$DATADIR/mysql" ]; then
-    if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
-        echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
-        echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
-        exit 1
-    fi
+if [[ ! -d "$VOLUME_HOME/mysql" ]]; then
+  if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
+    echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
+    echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
+    exit 1
+  fi
 
-    mkdir -p "$DATADIR"
-    chown -R mysql:mysql "$DATADIR"
+  mkdir -p "$VOLUME_HOME"
+  chown -R mysql:mysql "$VOLUME_HOME"
 
-    echo 'Initializing database'
-    mysql_install_db --user=mysql --datadir="$DATADIR" --rpm
-    echo 'Database initialized'
+  echo 'Initializing database'
+  mysql_install_db --datadir="$VOLUME_HOME"
+  echo 'Database initialized'
 
-    mysql --skip-networking &
-    pid="$!"
+  mysql=( mysqld_safe --protocol=socket -uroot )
+  RET=1
+  while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MariaDB service startup"
+    sleep 5
+    "${mysql[@]}" -e "status" > /dev/null 2>&1
+    RET=$?
+  done
 
-    mysql=( mysql --protocol=socket -uroot )
+  echo "SET @@SESSION.SQL_LOG_BIN=0;" | "${mysql[@]}"
+  echo "DELETE FROM mysql.user ;" | "${mysql[@]}"
+  echo "CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" | "${mysql[@]}"
+  echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;" | "${mysql[@]}"
+  echo "DROP DATABASE IF EXISTS test ;" | "${mysql[@]}"
+  echo "FLUSH PRIVILEGES ;" | "${mysql[@]}"
 
-    for i in {30..0}; do
-        if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
-            break
-        fi
-        echo 'MySQL init process in progress...'
-        sleep 1
-    done
-    if [ "$i" = 0 ]; then
-        echo >&2 'MySQL init process failed.'
-        exit 1
-    fi
-
-    if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]; then
-        # sed is for https://bugs.mysql.com/bug.php?id=20545
-        mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
-    fi
-
-	"${mysql[@]}" <<-EOSQL
-        -- What's done in this file shouldn't be replicated
-        --  or products like mysql-fabric won't work
-        SET @@SESSION.SQL_LOG_BIN=0;
-        DELETE FROM mysql.user ;
-        CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-        GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-        DROP DATABASE IF EXISTS test ;
-        FLUSH PRIVILEGES ;
-	EOSQL
 
     if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
         mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
     fi
 
     if [ "$MYSQL_DATABASE" ]; then
-        echo "RUNNING: CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
         echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
         mysql+=( "$MYSQL_DATABASE" )
     fi
 
     if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-        echo "RUNNING: CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
         echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
 
         if [ "$MYSQL_DATABASE" ]; then
-            echo "RUNNING: GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;"
             echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
         fi
 
-        echo 'RUNNING: FLUSH PRIVILEGES ;'
         echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
     fi
 
-    echo
     ls -la /docker-entrypoint-initdb.d/
     for f in /docker-entrypoint-initdb.d/*; do
         case "$f" in
-            *.sh)  echo "$0: running $f"; . "$f" ;;
-            *.sql) echo "$0: running $f"; "${mysql[@]}" < "$f" && echo ;;
-            *)     echo "$0: ignoring $f" ;;
+            *.sh)     echo "$0: running $f"; . "$f" ;;
+            *.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f" && echo ;;
+            *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
+            *)        echo "$0: ignoring $f" ;;
         esac
-        echo "$f"
     done
 
     if ! kill -s TERM "$pid" || ! wait "$pid"; then
@@ -97,6 +73,5 @@ if [ ! -d "$DATADIR/mysql" ]; then
 fi
 
 chown -R mysql:mysql "$DATADIR"
-echo "$@"
 
-exec "$@"
+exec "/usr/lib/systemd/systemd"
