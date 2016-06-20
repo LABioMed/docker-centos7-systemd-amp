f39d5a941fff09bc281a5f6e030e20aebeb8155d
Nicholas Alipaz <nalipaz@labiomed.org>
2016-06-20 15:21:16 -0700

    Mariadb, only run functions on first provision

--- mariadb/container/usr/local/bin/docker-entrypoint.sh
+++ mariadb/container/usr/local/bin/docker-entrypoint.sh
@@ -8,20 +8,21 @@ source mariadb-functions.sh
 ERROR_LOG="$VOLUME_HOME/error.log"
 MYSQLD_PID_FILE="$VOLUME_HOME/mysql.pid"
 
-# Trap INT and TERM signals to do clean DB shutdown
-#trap terminate_db SIGINT SIGTERM
+if [ ! -d ${VOLUME_HOME}/mysql ]; then
+    # Trap INT and TERM signals to do clean DB shutdown
+    #trap terminate_db SIGINT SIGTERM
 
-install_db
+    install_db
 
-/usr/bin/mysqld_safe & # Launch DB server in the background
-MYSQLD_SAFE_PID=$!
+    wait_for_db
+    show_db_status
+    create_db_user
+    import_db_files
+    secure_and_tidy_db
+    terminate_db
+fi
 
-wait_for_db
-show_db_status
-create_db_user
-import_db_files
-secure_and_tidy_db
-terminate_db
+
 
 # Execute the systemd command from base image.
 exec "/usr/lib/systemd/systemd"
